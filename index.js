f5d835322985ffade9c578bfe635eb4013e58a31
ahmetterturk <ahm.ert@hotmail.com>
2021-11-23 11:53:49 +1100

    before deloy

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
+
+// We can receive JSON data from POAT/PUT/PATCH/etc requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.send("Hello dfhnd");
+    res.json({ message: "This is a JSON message" });
 });
 
-app.listen(5000, () => {
+app.listen(PORT, HOST, () => {
     console.log("Server is running on port 5000");
 });
