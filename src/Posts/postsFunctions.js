f0d47092feeeb2405bb9e57eea1924fb95b1fdc2
ahmetterturk <ahm.ert@hotmail.com>
2021-12-06 11:54:26 +1100

    setup schema and get, post routes

--- src/Posts/postsFunctions.js
+++ src/Posts/postsFunctions.js
@@ -1,9 +1,35 @@
-function randomNumberGenerator() {
-    return Math.floor(Math.random() * 10);
+const { Post } = require("../database/schemas/PostSchema");
+
+// get all posts
+async function getAllPosts() {
+    let allPosts = await Post.find();
+    return JSON.stringify(allPosts);
 }
 
-async function someAsynchFunction() {
+// create a post
+async function createSpecificPost(postDetails) {
+    let newPost = new Post({
+        postTitle: postDetails.postTitle,
+        postContent: postDetails.postContent,
+        postAuthorID: postDetails.postAuthorID,
+        postRating: postDetails.postRating,
+    });
+
+    let creationResult = await newPost.save();
+    return creationResult;
+}
+
+function randomNumberGenerator() {
+    return 3;
+}
+
+async function someAsyncFunction() {
     return "some async thing";
 }
 
-module.exports = { randomNumberGenerator, someAsynchFunction };
+module.exports = {
+    randomNumberGenerator,
+    someAsyncFunction,
+    getAllPosts,
+    createSpecificPost,
+};
--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
+
+const { databaseConnector } = require("./database");
+
+const DATABASE_URI =
+    process.env.DATABASE_URI || "mongodb://localhost:27017/expressmongolesson";
+
+databaseConnector(DATABASE_URI)
+    .then(() => {
+        console.log("Database connected, yay!");
+    })
+    .catch((error) => {
+        console.log(`
+            Some error occured connecting to the database. It was:
+            ${error}
+         `);
+    });
 
 const firebaseAdmin = require("firebase-admin");
 firebaseAdmin.initializeApp({
